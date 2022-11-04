f619c01ca273182d6f5cdd6563280320ac310d65
s-SoMka-s <i.chiesov@gmail.com>
2022-11-04 11:38:09 +0700

    Update

--- part1/start.sh
+++ part1/start.sh
@@ -1,6 +1,6 @@
 fastqc e_coli_fastq.gz
-mv e_coli_fastqc.html e_coli.html
-rm e_coli_fastqc.zip
+mv e_coli_fastq_fastqc.html e_coli.html
+rm e_coli_fastq_fastqc.zip
 minimap2 -d ref.mmi ref_e_coli.fna.gz
 minimap2 -a ref.mmi e_coli_fastq.gz > alignment.sam
 samtools flagstat alignment.sam
