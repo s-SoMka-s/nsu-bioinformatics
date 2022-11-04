9fa0ead4ce01e78abd6a6b12fd41f4ff5f3de51c
i.chiesov <i.chiesov@gmail.com>
2022-11-04 14:21:51 +0700

    Update

--- part3/pipeline.go
+++ part3/pipeline.go
@@ -9,30 +9,30 @@ func main() {
 	wf := sp.NewWorkflow("workflow", 4)
 
 	src_ref := spc.NewFileSource(wf, "in_ref", "input/ref_e_coli.fna.gz")
-	src_source := spc.NewFileSource(wf, "in_source", "input/e_coli.fastq.gz")
+	src_source := spc.NewFileSource(wf, "in_source", "input/e_coli_fastq.gz")
 	src_parser := spc.NewFileSource(wf, "in_parser", "input/parser.py")
 
 	index := wf.NewProc("index", "minimap2 -d {o:ref} {i:in_ref}")
-	index.SetOut("ref", "ref.mmi")
 	index.In("in_ref").From(src_ref.Out())
+	index.SetOut("ref", "output/ref.mmi")
 
 	//fastqc := wf.NewProc("fastqc", "fastqc {i:in_source} > {o:fres}")
 	//fastqc.SetOut("fres", "./output/e_coli_fastq_fastqc.html")
 	//fastqc.In("in_source").From(src_source.Out())
 
 	alignment := wf.NewProc("alignment", "minimap2 -a {i:ref} {i:in_source} > {o:ares}")
-	alignment.SetOut("ares", "output/alignment.sam")
-	alignment.In("in_source").From(src_source.Out())
 	alignment.In("ref").From(index.Out("ref"))
+	alignment.In("in_source").From(src_source.Out())
+	alignment.SetOut("ares", "output/alignment.sam")
 
 	samtools := wf.NewProc("samtools", "samtools flagstat {i:ares} > {o:sres}")
-	samtools.SetOut("sres", "output/result.txt")
 	samtools.In("ares").From(alignment.Out("ares"))
+	samtools.SetOut("sres", "output/result.txt")
 
 	parse := wf.NewProc("parse", "python3 {i:in_parser} {i:sres} > {o:parsed}")
-	parse.SetOut("parsed", "output/parsed.txt")
 	parse.In("in_parser").From(src_parser.Out())
 	parse.In("sres").From(samtools.Out("sres"))
+	parse.SetOut("parsed", "output/parsed.txt")
 
 	// Run workflow
 	wf.Run()
